a09c536c4c489e2cccb402a038b838ac2c571bc5
Prashant <contactprsant@gmail.com>
2023-05-11 01:53:30 +0530

    update

--- src/data.js
+++ src/data.js
@@ -1,10 +1,9 @@
-
 export const featuredProjects = [
     {
         id: 1 ,
         title : "Crypto Pilot",
         desc : `This is a cryptocurrency project where you can get all the latest information in detail about all famous cryptocurrencies. You can read and search most recent news related to almost all important cryptocurrencies. I have used react-chartjs-2 so you can also see the price history of all important cryptocurrencies visually with the help of line chart for a variety of timelines. In this project I have used React Redux Toolkit for state management and React Query for fetching the data from 2 APIs.` ,
-        img: 'src/assets/project-images/react/crypto-pilot.JPG' ,
+        img: './assets/project-images/react/crypto-pilot.JPG' ,
         link : 'https://imprsant-cryptopilot.netlify.app/'
     },
 
@@ -12,7 +11,7 @@ export const featuredProjects = [
         id: 2 ,
         title : "Movies Master",
         desc : `Movies-Master is a react project for fetching all the details about movies and webseries. For fetching all these details I have used "The Movies Database (TMDB)" API with the help of axios. In this project you can fetch trending Movies, Movies list, TV series list and also you can search any movie or series. This is just a MVP right now and I have to add few more features. I hope you will love it.`,
-        img: 'src/assets/project-images/react/movies-master.JPG',
+        img: './assets/project-images/react/movies-master.JPG',
         link : 'https://moviesmaster.netlify.app/'
     },
     
@@ -20,7 +19,7 @@ export const featuredProjects = [
         id: 3 ,
         title : "Quiz App",
         desc : `Quiz Master is a quiz project made with react with more that 20 categories and 3 difficulty levels (easy, medium and hard). Play this game and test your knowledge. I hope you will like it.` ,
-        img: 'src/assets/project-images/react/quiz-app-react.png',
+        img: './assets/project-images/react/quiz-app-react.png',
         link : 'https://imprsant-quizmaster.netlify.app/'
     },
 
@@ -28,7 +27,7 @@ export const featuredProjects = [
         id: 4,
         title : "Words Web",
         desc : `This is a simple dictionary project created with the help of react (vite). Here you can search any word and find the meaning, synonyms and antonyms of that word. You can also listen the correct pronounciation of that word if the audio file is available for that word. I hope you will like it.` ,
-        img: 'src/assets/project-images/react/wordsweb-react.JPG',
+        img: './assets/project-images/react/wordsweb-react.JPG',
         link : "https://imprsant-words-web.netlify.app/"
     },
 
@@ -37,7 +36,7 @@ export const featuredProjects = [
         title : "Weather App",
         desc :`This is a simple weather app project created with the help of javascript, html and css. This project uses openweathermap API to fetch the weather data with the help of fetch api. I have also used navigator api in this project so it asks for permission to get your location (longitude and latitude) for showing the weather forcast and weather info.
         I hope you will love it.` ,
-        img: 'src/assets/project-images/javascript/weather-javascript.JPG',
+        img: './assets/project-images/javascript/weather-javascript.JPG',
         link : 'https://imprsant-weather.netlify.app/'
     },
 
@@ -46,7 +45,7 @@ export const featuredProjects = [
         title : "Shopping Cart",
         desc : `This is a fully functional shopping cart project made with the help of vanilla javascript. You can select how many items you want to add to cart and on cart section you can check all your selected items with the price of each and total items. You can also delete/remove the items from cart option.
         I have used localstorage so even if you refresh the web page your selected items will still be there.`,
-        img: 'src/assets/project-images/javascript/shoppingCart-javascript.JPG',
+        img: './assets/project-images/javascript/shoppingCart-javascript.JPG',
         link : 'https://imprsant.github.io/shopping-cart/'
     },
 
@@ -54,7 +53,7 @@ export const featuredProjects = [
         id: 7 ,
         title : "Tic Tak Toe Game",
         desc : 'This is a simple tic tak toe game made in pure javascript. I hope you will love it.' ,
-        img: 'src/assets/project-images/javascript/tic tak toe.JPG',
+        img: './assets/project-images/javascript/tic tak toe.JPG',
         link : 'https://imprsant.github.io/Tic-tak-toe/'
     },
 ]
@@ -65,7 +64,7 @@ export const reactProjects = [
         id: 1 ,
         title : "Crypto Pilot",
         desc : `This is a cryptocurrency project where you can get all the latest information in detail about all famous cryptocurrencies. You can read and search most recent news related to almost all important cryptocurrencies. I have used react-chartjs-2 so you can also see the price history of all important cryptocurrencies visually with the help of line chart for a variety of timelines. In this project I have used React Redux Toolkit for state management and React Query for fetching the data from 2 APIs.` ,
-        img: 'src/assets/project-images/react/crypto-pilot.JPG' ,
+        img: './assets/project-images/react/crypto-pilot.JPG' ,
         link : 'https://imprsant-cryptopilot.netlify.app/'
     },
    
@@ -73,7 +72,7 @@ export const reactProjects = [
         id: 2 ,
         title : "Movies Master",
         desc : `Movies-Master is a react project for fetching all the details about movies and webseries. For fetching all these details I have used "The Movies Database (TMDB)" API with the help of axios. In this project you can fetch trending Movies, Movies list, TV series list and also you can search any movie or series. This is just a MVP right now and I have to add few more features. I hope you will love it.` ,
-        img: 'src/assets/project-images/react/movies-master.JPG',
+        img: './assets/project-images/react/movies-master.JPG',
         link : 'https://moviesmaster.netlify.app/'
     },
     
@@ -81,7 +80,7 @@ export const reactProjects = [
         id: 3 ,
         title : "Quiz App",
         desc : `Quiz Master is a quiz project made with react with more that 20 categories and 3 difficulty levels (easy, medium and hard). Play this game and test your knowledge. I hope you will like it.`,
-        img: 'src/assets/project-images/react/quiz-app-react.png',
+        img: './assets/project-images/react/quiz-app-react.png',
         link : 'https://imprsant-quizmaster.netlify.app/'
     },
 
@@ -89,7 +88,7 @@ export const reactProjects = [
         id: 4,
         title : "Words Web",
         desc : `This is a simple dictionary project created with the help of react (vite). Here you can search any word and find the meaning, synonyms and antonyms of that word. You can also listen the correct pronounciation of that word if the audio file is available for that word. I hope you will like it.` ,
-        img: 'src/assets/project-images/react/wordsweb-react.JPG',
+        img: './assets/project-images/react/wordsweb-react.JPG',
         link : "https://imprsant-words-web.netlify.app/"
     },
 
@@ -97,7 +96,7 @@ export const reactProjects = [
         id: 5 ,
         title : "React From Validation",
         desc : `This is a simple react project created to learn form handeling and validation in react without using any library.` ,
-        img: 'src/assets/project-images/react/form-validation-react.JPG',
+        img: './assets/project-images/react/form-validation-react.JPG',
         link:"https://imprsant-reactform.netlify.app/"
     },
 
@@ -105,7 +104,7 @@ export const reactProjects = [
         id: 6 ,
         title : "Todo (PWA)",
         desc : `This is a simple todo progressive web app made in react. I have used local-storage so your added to-dos will not get erased until you delete them or clear the cache of your browser.`,
-        img: 'src/assets/project-images/react/react-todo.JPG',
+        img: './assets/project-images/react/react-todo.JPG',
         link : 'https://todoapp-v1-imprsant.netlify.app/'
     },
 
@@ -113,7 +112,7 @@ export const reactProjects = [
         id: 7 ,
         title : "React Auto-search",
         desc : `This is a very simple and small project of auto-search in react made with the help of vite. In this project if you type any string in search-bar, a list of suggested keywords will automatically generated that contains the typed string.` ,
-        img: 'src/assets/project-images/react/autosearch-react.JPG',
+        img: './assets/project-images/react/autosearch-react.JPG',
         link : 'https://imprsant-autosearch.netlify.app/'
     },
 
@@ -121,7 +120,7 @@ export const reactProjects = [
         id: 8 ,
         title : "React Pagination Project",
         desc :`This is a very basic project of pagination made in react. I have used dummyjson api for fetching fake data with the help of fetch api. I hope you will love it. I am open for any suggestion.` ,
-        img: 'src/assets/project-images/react/pagination.JPG',
+        img: './assets/project-images/react/pagination.JPG',
         link: 'https://imprsant-pagination.netlify.app/'
     },
 ]
@@ -134,14 +133,14 @@ export const javascriptProject = [
         title : "Weather App",
         desc : `This is a simple weather app project created with the help of javascript, html and css. This project uses openweathermap API to fetch the weather data with the help of fetch api. I have also used navigator api in this project so it asks for permission to get your location (longitude and latitude) for showing the weather forcast and weather info.
         I hope you will love it.` ,
-        img: 'src/assets/project-images/javascript/weather-javascript.JPG',
+        img: './assets/project-images/javascript/weather-javascript.JPG',
         link : 'https://imprsant-weather.netlify.app/'
     },
     {
         id: 2 ,
         title : "Quiz App",
         desc : 'This is a very simple pagination project made in react.' ,
-        img:'src/assets/project-images/javascript/quiz-javascript.JPG',
+        img:'./assets/project-images/javascript/quiz-javascript.JPG',
         link : 'https://imprsant.github.io/quiz-app/'
     },
     {
@@ -149,63 +148,63 @@ export const javascriptProject = [
         title : "Shopping Cart",
         desc : `This is a fully functional shopping cart project made with the help of vanilla javascript. You can select how many items you want to add to cart and on cart section you can check all your selected items with the price of each and total items. You can also delete/remove the items from cart option.
         I have used localstorage so even if you refresh the web page your selected items will still be there.`,
-        img: 'src/assets/project-images/javascript/shoppingCart-javascript.JPG',
+        img: './assets/project-images/javascript/shoppingCart-javascript.JPG',
         link : 'https://imprsant.github.io/shopping-cart/'
     },
     {
         id: 4 ,
         title : "Tic Tak Toe Game",
         desc : 'This is a simple tic tak toe game made in pure javascript. I hope you will love it.' ,
-        img: 'src/assets/project-images/javascript/tic tak toe.JPG',
+        img: './assets/project-images/javascript/tic tak toe.JPG',
         link : 'https://imprsant.github.io/Tic-tak-toe/'
     },
     {
         id: 5 ,
         title : "Nested Comment",
         desc : `This is a very very basic project of infinite level nested comment.` ,
-        img: 'src/assets/project-images/javascript/Nested-Comment.JPG',
+        img: './assets/project-images/javascript/Nested-Comment.JPG',
         link : 'https://imprsant.github.io/Nested-Comment-in-javascript/'
     },
     {
         id: 6 ,
         title : "Multi-step Form",
         desc : `This is a very basic and simple multi-step form where you can add your details in muliple steps.` ,
-        img: 'src/assets/project-images/javascript/mult-step-form-javascript.JPG',
+        img: './assets/project-images/javascript/mult-step-form-javascript.JPG',
         link : 'https://imprsant.github.io/Multi-step-form/'
     },
     {
         id: 7 ,
         title : "Stopwatch",
         desc : `Simple stopwatch project` ,
-        img: 'src/assets/project-images/javascript/stopwatch-javascript.JPG',
+        img: './assets/project-images/javascript/stopwatch-javascript.JPG',
         link : 'https://imprsant.github.io/stopwatch/'
     },
     {
         id: 8 ,
         title : "Mosquito Hit Game",
         desc :`This is a simple javascript game based project where you can hit the mosquito appearing randomly at the screen. Let's see how many points you can make.`,
-        img: 'src/assets/project-images/javascript/mosquito-hit-javascript.JPG',
+        img: './assets/project-images/javascript/mosquito-hit-javascript.JPG',
         link : 'https://imprsant.github.io/Mosquito-Hit-Game/'
     },
     {
         id: 9 ,
         title : "Quote Generator",
         desc :`This is a simple DOM based project to generate a random quote.`,
-        img: 'src/assets/project-images/javascript/quoto-generator-javascript.JPG',
+        img: './assets/project-images/javascript/quoto-generator-javascript.JPG',
         link : 'https://imprsant.github.io/Quote-generator/'
     },
     {
         id: 10 ,
         title : "Accordion",
         desc : `This is a simple and basic accordion project just to practice some DOM concepts.` ,
-        img: 'src/assets/project-images/javascript/accordion-javascript.JPG',
+        img: './assets/project-images/javascript/accordion-javascript.JPG',
         link : 'https://imprsant.github.io/Accordion/'
     },
     {
         id: 11 ,
         title : "Grade Calculator",
         desc : `This is a simple Grade Calculator project based on the given marks`,
-        img: 'src/assets/project-images/javascript/grade-calculator-javascript.JPG',
+        img: './assets/project-images/javascript/grade-calculator-javascript.JPG',
         link : 'https://imprsant.github.io/Grade-Calculator/'
     },
 
