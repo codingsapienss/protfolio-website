f71da2f6ebcaad4fe370cbeb6f090761b158ee07
Prashant <contactprsant@gmail.com>
2023-06-22 19:56:42 +0530

    style_update

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -2,16 +2,12 @@ import './Topbar.scss'
 
 const topbar = () => {
   return (
-
-
     <div id='topbar' className='topbar'>
       <a href="#intro" className='logo'>@imprsant
       </a>
 
       <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/16y89aYIi8mpdt0fvIRkd7G414jwPTh-P/view?usp=drive_link" > Resume
       </a>
-
-
     </div>
   )
 }
