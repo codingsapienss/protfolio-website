09c247a6f72d0e03c1e6fc508a8ea82dcde65bc4
Prashant Sharma <93860018+imprsant@users.noreply.github.com>
2023-07-30 10:09:39 +0530

    Update Topbar.jsx

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,7 +3,7 @@ import './Topbar.scss'
 const topbar = () => {
   return (
     <div id='topbar' className='topbar'>
-      <a href="#intro" className='logo'>@imprsant
+      <a href="#intro" className='logo'>Coding Sapiens
       </a>
 
       <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/16y89aYIi8mpdt0fvIRkd7G414jwPTh-P/view?usp=drive_link" > Resume
