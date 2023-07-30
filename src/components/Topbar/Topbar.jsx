a28c3177e292b87041c8082cac7ed1ac545da07c
Prashant Sharma <93860018+codingsapienss@users.noreply.github.com>
2023-07-30 16:01:27 +0530

    Update Topbar.jsx

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -6,7 +6,7 @@ const topbar = () => {
       <a href="#intro" className='logo'>Coding Sapiens
       </a>
 
-      <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/16y89aYIi8mpdt0fvIRkd7G414jwPTh-P/view?usp=drive_link" > Resume
+      <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/1fh_aFvK-R-mw8YK_BMPtZsYfPZ6gqxol/view?usp=drive_link" > Resume
       </a>
     </div>
   )
