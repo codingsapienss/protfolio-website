86763e9e5a6d6baf1f6bc60ebdc3a9a69e92b041
Prashant <contactprsant@gmail.com>
2023-09-25 13:02:41 +0530

    Update UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,22 @@ import Topbar from './components/Topbar/Topbar'
 import Intro from './components/Intro/Intro'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
-import Stars from './components/Stars/Stars'
+import Star from './components/Star/Star'
 
 const App = () => {
   return (
 
     <>
-      <Stars />
-
-
+      <Star />
       <div className='app'>
-
         <Topbar />
-
         <div className="section">
           <Intro />
           <div className="other">
             <Portfolio />
-
             <Contact className='contact' />
           </div>
         </div>
-
-
       </div>
 
     </>
---
+++ src/components/Star/Star.jsx
@@ -0,0 +1,9 @@
+import './Star.css'
+
+const Star = () => {
+    return (
+        <img className="star" src='\stars.png' />
+    )
+}
+
+export default Star
