4a259682115c8794606192b05d6066f892ed60d2
Prashant <contactprsant@gmail.com>
2023-09-25 12:03:24 +0530

    Update UI

--- src/components/Stars/Stars.jsx
+++ src/components/Stars/Stars.jsx
@@ -30,9 +30,8 @@ const Stars = () => {
                     style={{
                         left: star.left,
                         bottom: star.bottom,
-
                     }}
-                ></div>
+                > * </div>
             ))}
         </div>
     );
