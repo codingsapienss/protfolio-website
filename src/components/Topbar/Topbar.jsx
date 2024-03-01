9db370d39adafb43a1dd3346cf772a7707a2c77b
codingsapienss <contactprsant@gmail.com>
2024-03-01 08:51:38 +0530

    updates

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,15 +1,16 @@
-import './Topbar.scss'
+import "./Topbar.scss";
 
 const topbar = () => {
   return (
-    <div id='topbar' className='topbar'>
-      <a href="#intro" className='logo'>Coding Sapiens
+    <div id="topbar" className="topbar">
+      <a href="#intro" className="logo">
+        Coding Sapiens
       </a>
 
-      <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/16y89aYIi8mpdt0fvIRkd7G414jwPTh-P/view?usp=share_link" > Resume
-      </a>
+      {/* <a target='_blank' rel="noreferrer" id='resume' href="https://drive.google.com/file/d/16y89aYIi8mpdt0fvIRkd7G414jwPTh-P/view?usp=share_link" > Resume
+      </a> */}
     </div>
-  )
-}
+  );
+};
 
-export default topbar
+export default topbar;
